251f672ad427eddd59cef3c76bcc95bdb24074a6
Victoria Patteson <pattersv@oregonstate.edu>
2022-10-16 12:01:49 -0700

    made change

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, json, redirect
+from flask import request
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect("/home.j2")
+    return "hello peanut"

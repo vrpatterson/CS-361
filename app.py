a9b706bfa81f6937104f2df397c45d2c68c6211d
Victoria Patteson <pattersv@oregonstate.edu>
2022-10-16 13:06:06 -0700

    changes

--- app.py
+++ app.py
@@ -5,5 +5,18 @@ import os
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return redirect('home')
+    return render_template('home.j2')
+
+@app.route("/find-peak")
+def findPeak():
+    return render_template('find-peak.j2')
+
+@app.route("/faq")
+def faq():
+    return render_template('faq.j2')
+
+@app.route("/search")
+def search():
+    return render_template('search.j2')
